7c3679a7692e410231e476aad1f08164e8bf9cf8
nicolas <nicolas.matteazzi@gmail.com>
2017-04-27 08:19:21 +0200

    Concept - main page restyle

--- traveler-concept/WebContent/js/home.js
+++ traveler-concept/WebContent/js/home.js
@@ -12,9 +12,9 @@ initMap = function() {
 	];
 	
 	map = new google.maps.Map(document.getElementById('mapdiv'), {
-	    center: {lat: 45.475757, lng: 9.168083},
+	    center: {lat: 19.475757, lng: 9.168083},
 	    scrollwheel: false,
-	    zoom: 2,
+	    zoom: 1,
 	    streetViewControl: false,
 	    mapTypeControl: false
 	});
@@ -36,10 +36,12 @@ homeModule.controller("homeController", [ '$scope', '$http', '$timeout', '$mdDia
 		$scope.storyline = new Object();
 		$scope.storyline.days = [];
 		$scope.storyline.days.push({day: new Date(moment("02/04/2017", "DD/MM/YYYY")), feeds: []})
+		$scope.storyline.days.push({day: new Date(moment("01/04/2017", "DD/MM/YYYY")), feeds: []})
 		
-//		$scope.storyline.days[0].feeds.push({avatar: 'angelina', who: 'Angelina', where: 'Singapore', type: 'search', from: new Date(moment("01/08/2017", "DD/MM/YYYY"), to: new Date(moment("12/08/2017", "DD/MM/YYYY"))});
-//		$scope.storyline.days[0].feeds.push({avatar: 'ryan', who: 'Ryan', type: 'leaves', where: 'New York', from: new Date(moment("21/07/2017", "DD/MM/YYYY"), to: new Date(moment("28/07/2017", "DD/MM/YYYY"))});
-//		$scope.storyline.days[0].feeds.push({avatar: 'jennifer', who: 'Jennifer', , type: 'tip', tipType: 'food'});
+		$scope.storyline.days[0].feeds.push({avatar: 'ryan', who: 'Ryan', type: 'leaves', where: 'New York', from: new Date(moment("21/07/2017", "DD/MM/YYYY")), to: new Date(moment("28/07/2017", "DD/MM/YYYY")), showMenu: false});
+		$scope.storyline.days[0].feeds.push({avatar: 'angelina', who: 'Angelina', type: 'search', where: 'Singapore', from: new Date(moment("03/08/2017", "DD/MM/YYYY")), to: new Date(moment("28/08/2017", "DD/MM/YYYY")), showMenu: false});
+		
+		$scope.storyline.days[1].feeds.push({avatar: 'jennifer', who: 'Jennifer', type: 'search', where: 'New York', from: new Date(moment("02/07/2017", "DD/MM/YYYY")), to: new Date(moment("15/07/2017", "DD/MM/YYYY")), showMenu: false});
 	}
 	
 	/**
